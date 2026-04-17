4b0e29df65712b3424d6bdc78191978e8e9e8ec8
Trevoroteyo <tserwanga17@gmail.com>
2026-04-17 11:55:01 +0300

    feat: Posthog analytics intergration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,14 @@ import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
+import { PostHogProvider } from "posthog-react-native";
 import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const posthogApiKey = process.env.EXPO_PUBLIC_POSTHOG_API_KEY!;
+const posthogHost = process.env.EXPO_PUBLIC_POSTHOG_HOST;
 
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
@@ -34,8 +37,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack screenOptions={{ headerShown: false }} />
-    </ClerkProvider>
+    <PostHogProvider apiKey={posthogApiKey} options={{ host: posthogHost }}>
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ClerkProvider>
+    </PostHogProvider>
   );
 }
